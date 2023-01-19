a831d6b5693423dc67ae54ff88b35ba02f853230
unknown <rygailkim@gmail.com>
2023-01-19 11:49:02 +0800

    add account

--- pages/add-account.php
+++ pages/add-account.php
@@ -1,7 +1,106 @@
-<?php include('../partials/header.php') ?>
+<?php
+require_once "../config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = $name = $site = $category = $description = "";
+$username_err = $password_err = $name_err = $site_err = $category_err = $description_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate username/email
+    $input_username = trim($_POST["username"]);
+    if(empty($input_username)){
+        $username_err = "Please enter a username/email.";
+    } else{
+        $username = $input_username;
+    }
+
+    // Validate password
+    $input_password = trim($_POST["password"]);
+    if(empty($input_password)){
+        $password_err = "Please enter a password.";
+    } else{
+        $password = $input_password;
+    }
+
+    // Validate account name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Please enter the account name.";
+    } else{
+        $name = $input_name;
+    }
+
+    // Validate website URL
+    $input_site = trim($_POST["site"]);
+    if(empty($input_site)){
+        $site_err = "Please enter the website's URL.";
+    } else{
+        $site = $input_site;
+    }
+
+    // Validate account category
+    $input_category = trim($_POST["category"]);
+    if(empty($input_site)){
+        $category_err = "Please enter the account category.";
+    } else{
+        $category = $input_category;
+    }
+
+    // Validate account description
+    $input_description = trim($_POST["description"]);
+    $description = $input_description;
+    
+    // Check input errors before inserting in database
+    if(empty($username_err) && empty($password_err) && empty($name_err) && empty($site_err) && empty($category_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO accounts (username, password, name, site, category, description)
+          VALUES (?, ?, ?, ?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            if(!$stmt) {
+              echo 'mysqli error';
+            }
+
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ssssss", $param_username, $param_password, $param_name,
+              $param_site, $param_category, $param_description);
+            
+            // Set parameters
+            $param_username = $username;
+            $param_password = $password;
+            $param_name = $name;
+            $param_site = $site;
+            $param_category = $category;
+            $param_description = $description;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records created successfully. Redirect to landing page
+                header("location: dashboard.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+          // Close statement
+          mysqli_stmt_close($stmt);
+        }
+        else {
+          echo "Something's wrong with the query: " . mysqli_error($link);
+      }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+
+include('../partials/header.php');
+include('../partials/sidebar.php');
+
+?>
 
 <body class="g-sidenav-show  bg-gray-100">
-<?php include('../partials/sidebar.php') ?>
 
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
     <!-- Navbar -->
@@ -12,29 +111,33 @@
       <div class="row">
         <div class="col-12">
           <div class="card p-4">
-            <form>
+          <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
               <div class="form-group">
                   <label for="example-text-input" class="form-control-label">Username/Email</label>
-                  <input class="form-control" type="text" value="" id="example-text-input">
+                  <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                  <span class="invalid-feedback"><?php echo $username_err;?></span>
               </div>
               <div class="form-group">
                   <label for="example-password-input" class="form-control-label">Password</label>
                   <div class="mb-3 pw">
-                    <input type="password" class="form-control password" id="password" aria-label="Password" aria-describedby="password-addon">
-                    <button class="input-group-button btn btn-light border password_show" id="password_show" type="button" onclick="toggle_password()"><i class="fa fa-eye-slash"></i></button>
+                    <input type="text" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                    <!-- <button class="input-group-button btn btn-light border password_show" id="password_show" type="button" onclick="toggle_password()"><i class="fa fa-eye-slash"></i></button> -->
+                    <span class="invalid-feedback"><?php echo $password_err;?></span>
                   </div>
               </div>
               <div class="form-group">
                 <label for="example-text-input" class="form-control-label">Account Name</label>
-                <input class="form-control" type="text" value="" id="example-text-input">
+                <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
+                <span class="invalid-feedback"><?php echo $name_err;?></span>
               </div>
               <div class="form-group">
                 <label for="example-url-input" class="form-control-label">Account URL</label>
-                <input class="form-control" type="url" value="" id="example-url-input">
+                <input type="text" name="site" class="form-control <?php echo (!empty($site_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $site; ?>">
+                <span class="invalid-feedback"><?php echo $site_err;?></span>
               </div>
               <div class="form-group">
                 <label for="exampleFormControlSelect1">Account Category</label>
-                <select class="form-control" id="exampleFormControlSelect1">
+                <select class="form-control" name="category" value="<?php echo $category; ?>">
                   <option>Social</option>
                   <option>School</option>
                   <option>Work</option>
@@ -46,12 +149,13 @@
               </div>
               <div class="form-group">
                 <label for="exampleFormControlTextarea1">Description/Notes</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <textarea name="description" class="form-control" value=""><?php echo $description; ?></textarea>
               </div>
               <div class="row mt-6">
                 <div class="col">
                   <div class="text-center">
-                    <button type="button" class="btn bg-gradient-success w-100" onclick="">Save Changes</button>
+                    <!-- <button type="button" class="btn bg-gradient-success w-100" onclick="">Save Changes</button> -->
+                    <input type="submit" class="btn bg-gradient-success w-100" value="Save Changes">
                   </div>
                 </div>
                 <div class="col">
--- pages/dashboard.php
+++ pages/dashboard.php
@@ -2,14 +2,57 @@
 
 <?php
 
-// Initialize the session
-session_start();
- 
-// Check if the user is logged in, if not then redirect him to sign-in page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: sign-in.php");
-    exit;
+// Check existence of id parameter before processing further
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+  // Include config file
+  require_once "config.php";
+  
+  // Prepare a select statement
+  $sql = "SELECT * FROM employees WHERE id = ?";
+  
+  if($stmt = mysqli_prepare($link, $sql)){
+      // Bind variables to the prepared statement as parameters
+      mysqli_stmt_bind_param($stmt, "i", $param_id);
+      
+      // Set parameters
+      $param_id = trim($_GET["id"]);
+      
+      // Attempt to execute the prepared statement
+      if(mysqli_stmt_execute($stmt)){
+          $result = mysqli_stmt_get_result($stmt);
+  
+          if(mysqli_num_rows($result) == 1){
+              /* Fetch result row as an associative array. Since the result set
+              contains only one row, we don't need to use while loop */
+              $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+              
+              // Retrieve individual field value
+              $name = $row["name"];
+              $address = $row["address"];
+              $salary = $row["salary"];
+          } else{
+              // URL doesn't contain valid id parameter. Redirect to error page
+              echo "Something's wrong with the query: " . mysqli_error($link);
+              exit();
+          }
+          
+      } else{
+          echo "Oops! Something went wrong. Please try again later.";
+      }
+  }
+   
+  // Close statement
+  mysqli_stmt_close($stmt);
+  
+  // Close connection
+  mysqli_close($link);
+} else{
+  // URL doesn't contain id parameter. Redirect to error page
+  echo "Something's wrong with the query: " . mysqli_error($link);
+  exit();
 }
+
+
 include('../partials/header.php')
 
 ?>
